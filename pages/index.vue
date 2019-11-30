36ce7eb75ff9049fcb0afeb87485a0130af0169b
mitsudaman <k.m.masato-uver-aquarius@hotmail.co.jp>
2019-11-30 11:41:00 +0900

    ページ遷移機能追加、SPA化

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,57 @@
 <template>
-  <b-container class="bv-example-row mt-5">
-    <b-row class="justify-content-md-center">
-      <b-col cols="5">
-        <draggable v-model="List" group="people" @start="drag=true" @end="drag=false">
-        <b-card v-for="element in List" :key="element.id" class="mb-2">
-          <b-card-text>{{element.text}}
-          </b-card-text>
-        </b-card> 
-      </draggable>
-      </b-col>
-    </b-row>
-    {{List}}
-    <b-row class="justify-content-md-center">
-      <b-col cols="8">
-        <chart :param="param"></chart>
-      </b-col>
-    </b-row>
+  <b-container class="mt-5">
+    <h2 class="mb-5">Q.あなたにとって優先度の高い順番で並び替えてください。</h2>
+    <template v-if="pageIndex==1">
+      <b-row class="justify-content-md-center">
+        <b-col xs="12" md="5">
+          <draggable v-model="List1" group="people" @start="drag=true" @end="drag=false">
+          <b-card v-for="(element,index) in List1" :key="element.id" class="mb-4">
+            <b-card-text>{{index + 1}}位 {{element.text}}
+            </b-card-text>
+          </b-card> 
+        </draggable>
+        </b-col>
+      </b-row>
+      {{List1}}
+    </template>
+    <template v-if="pageIndex==2">
+      <b-row class="justify-content-md-center">
+        <b-col xs="12" md="5">
+          <draggable v-model="List2" group="people" @start="drag=true" @end="drag=false">
+          <b-card v-for="(element,index) in List2" :key="element.id" class="mb-4">
+            <b-card-text>{{index + 1}}位 {{element.text}}
+            </b-card-text>
+          </b-card> 
+        </draggable>
+        </b-col>
+      </b-row>
+      {{List2}}
+    </template>
+   <div>
+      <b-row class="justify-content-md-center mt-5">
+        <b-col xs="12" md="5">
+          <b-row class="justify-content-md-center">
+            <b-col cols="5">
+              <b-button 
+              v-if="pageIndex>1"
+              block variant="outline-primary" @click="back()">前へ</b-button>
+            </b-col>
+            <b-col cols="2">
+            </b-col>
+            <b-col cols="5">
+              <b-button v-if="pageIndex<4" block variant="outline-primary" @click="next()">次へ</b-button>
+              <b-button v-if="pageIndex==4" block variant="outline-primary" @click="result()">結果を見る</b-button>
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+    </div>
   </b-container>
 </template>
 
 <script>
 import draggable from 'vuedraggable'
-import Chart from './Chart';
+import Chart from '@/Components/Chart';
 
 export default {
   components: {
@@ -30,7 +60,7 @@ export default {
   },
   data () {
     return {
-      List: [
+      List1: [
         {
           text: '休み',
           id: 1
@@ -44,8 +74,36 @@ export default {
           id: 3
         }
       ],
+      List2: [
+        {
+          text: '地位',
+          id: 1
+        },
+        {
+          text: '名誉',
+          id: 2
+        },
+        {
+          text: '肩書き',
+          id: 3
+        }
+      ],
       selectedIndex: 0,
-      param: [100, 70, 75, 90, 95]
+      pageIndex:1
+    }
+  },
+  methods: {
+    back(){
+      this.pageIndex -- 
+      console.log(this.pageIndex)
+    },
+    next(){
+      this.pageIndex ++ 
+      console.log(this.pageIndex)
+    },
+    result(){
+      console.log("aaaaa")
+      this.$router.push('result')
     }
   }
 }
---
+++ pages/result.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-container class="bv-example-row mt-5">
+    <h2 class="mb-5">あなたの価値観は・・・</h2>
+    <b-row class="justify-content-md-center">
+      <b-col cols="8">
+        <chart :param="param"></chart>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+import Chart from '@/Components/Chart';
+
+export default {
+  components: {
+    draggable,
+    Chart
+  },
+  data () {
+    return {
+      selectedIndex: 0,
+      param: [100, 70, 75, 90, 95]
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  /* display: flex; */
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+</style>
+
